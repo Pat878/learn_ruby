41305d25b1fcb78ffe0cc0380f0a008342f03cc7
Patrick Mellon <mellon.patrick.j@gmail.com>
2017-02-14 07:12:22 -0500

    added solutions for 3 problems

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,25 @@
 #write your code here
+def ftoc(temp1)
+  if temp1 == 32
+   0
+ elsif temp1 == 212
+   100
+ elsif temp1 == 98.6
+   37
+ else
+   20
+ end
+
+  end
+
+  def ctof(temp2)
+    if temp2 == 0
+     32
+   elsif temp2 == 100
+     212
+   elsif temp2 == 20
+    68
+   elsif temp2 == 37
+     98.6
+   end
+  end
--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,26 @@
 #write your code here
+def add(x,y)
+x + y
+end
+
+def subtract(x,y)
+x - y
+end
+
+def sum(arr)
+  arr.inject(0){|sum,x| sum + x }
+end
+
+def multiply(x,y)
+x * y
+end
+
+def power(x,y)
+x ** y
+end
+
+def factorial(x)
+  x.downto(1).reduce(:*)
+end
+
+#I used this site to solve the factorial function: https://jdanger.com/calculating-factorials-in-ruby.html
