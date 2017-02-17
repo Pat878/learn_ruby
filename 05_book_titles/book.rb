9e280ed291f88d638baa95112dbee7bb91d52af8
Patrick Mellon <mellon.patrick.j@gmail.com>
2017-02-17 07:18:56 -0500

    added solutions

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,11 +1,17 @@
 class Book
 
-
   def title=(book)
-     @title = book.titleize
-   end
+    small_words = %w{a an and the or for of nor in}
 
-   def title
-     @title
-   end
- end
+    @title = book.split.each_with_index.map{|word, index| small_words.include?(word) && index > 0 ? word : word.capitalize }.join(" ")
+
+  end
+
+  def title
+    @title
+  end
+
+end
+
+
+#https://stackoverflow.com/questions/17635030/how-do-i-capitalize-all-words-in-a-string-apart-from-small-words-in-the-middle-a
