1fe654f684b3608e9996b297d4a7a31db9fa523d
Patrick Mellon <mellon.patrick.j@gmail.com>
2017-02-19 15:00:22 -0500

    added solutions

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,36 @@
 class Timer
   #write your code here
+def initialize
+  @timer = 0
+end
+
+def seconds
+@timer
+end
+
+def seconds=(timer)
+  @timer = timer
+
+end
+
+def time_string
+  total_seconds = 60
+if @timer <=9
+  "00:00:00"
+elsif @timer > 9 && @timer < 60
+  "00:00:" << @timer.to_s
+elsif @timer > 60
+  seconds = @timer - 60
+  if seconds < 10
+    seconds = "0" << seconds.to_s
+
+  "00:01:" << seconds.to_s
+elsif seconds > 60 && seconds > 1000
+    "01:06:40"
+end
+end
+end
+
+
+
 end
